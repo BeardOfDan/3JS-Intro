16466eab29f870fce222607b09470b6a2e738e96
Daniel Roberts <BeardOfDan@gmail.com>
2018-09-27 19:14:20 -0700

    (feat) Change background color
    Was black, now it's gray

--- js/main.js
+++ js/main.js
@@ -1,27 +1,33 @@
 
-// Three JS set up
 const scene = new THREE.Scene();
+scene.background = new THREE.Color(0x909090);
 const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
+camera.position.z = 5;
 
 const renderer = new THREE.WebGLRenderer();
 renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
-// Things to render with Three JS
-const geometry = new THREE.BoxGeometry(1, 1, 1);
-const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+// Cube
+let geometry = new THREE.BoxGeometry(1, 1, 1);
+let material = new THREE.MeshBasicMaterial({
+  color: 0x00ff00,
+  transparent: true,
+  opacity: 0.75
+});
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
-camera.position.z = 5;
+
+
 
 function animate() { // runs with frequency of frame rate
-    requestAnimationFrame(animate);
+  requestAnimationFrame(animate);
 
-    cube.rotation.x += 0.01;
-    cube.rotation.y += 0.01;
-    cube.rotation.z += 0.01;
+  cube.rotation.x += 0.01;
+  cube.rotation.y += 0.01;
+  cube.rotation.z += 0.01;
 
-    renderer.render(scene, camera);
+  renderer.render(scene, camera);
 }
 animate();

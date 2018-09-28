54b92d9fc941c4b833be4d37c2d67bc16bb61212
Daniel Roberts <BeardOfDan@gmail.com>
2018-09-27 21:17:30 -0700

    (feat) Display a smaller box inside a, transparent, larger one

--- js/main.js
+++ js/main.js
@@ -9,15 +9,24 @@ renderer.setSize(window.innerWidth, window.innerHeight);
 document.body.appendChild(renderer.domElement);
 
 // Cube
-let geometry = new THREE.BoxGeometry(1, 1, 1);
+let geometry = new THREE.BoxGeometry(3, 3, 3);
 let material = new THREE.MeshBasicMaterial({
   color: 0x00ff00,
   transparent: true,
-  opacity: 0.75
+  opacity: 0.25
+  // wireframe: true
 });
 const cube = new THREE.Mesh(geometry, material);
 scene.add(cube);
 
+geometry = new THREE.BoxGeometry(1, 1, 1);
+material = new THREE.MeshBasicMaterial({
+  color: 0x0000ff,
+  transparent: false
+  // opacity: 0.5
+});
+const innerCube = new THREE.Mesh(geometry, material);
+scene.add(innerCube);
 
 
 
@@ -27,6 +36,10 @@ function animate() { // runs with frequency of frame rate
   cube.rotation.x += 0.01;
   cube.rotation.y += 0.01;
   cube.rotation.z += 0.01;
+
+  innerCube.rotation.x -= 0.05;
+  innerCube.rotation.y -= 0.05;
+  innerCube.rotation.z -= 0.05;
 
   renderer.render(scene, camera);
 }

9e21bf6c55372ee59fbe5b90c9af47cf47594c95
Daniel Roberts <BeardOfDan@gmail.com>
2018-09-30 17:20:47 -0700

    (feat) Create flip sort logic

--- js/main.js
+++ js/main.js
@@ -31,51 +31,60 @@ const alpha = new THREE.Mesh(geometry, material);
 alpha.translateX(-1);
 scene.add(alpha);
 
-// beta
-const beta = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({
-  color: 0xff0000,
-  transparent: false,
-  wireframe: true
-  // opacity: 0.5
-}));
-scene.add(beta);
-
-// gamma
-const gamma = new THREE.Mesh(new THREE.BoxGeometry(1, 1, 1), new THREE.MeshBasicMaterial({
-  color: 0x0000ff,
-  transparent: false,
-  wireframe: true
-  // opacity: 0.5
-}));
-gamma.translateX(1);
-scene.add(gamma);
-
 
 const group1 = new THREE.Object3D();
 group1.add(alpha);
-group1.add(beta);
-group1.add(gamma);
+// group1.add(beta);
+// group1.add(gamma);
 group1.translateY(-0.75);
 scene.add(group1);
 
-const group2 = group1.clone();
-group2.translateY(1.5);
-scene.add(group2);
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
 
-const hallway = new THREE.Mesh(new THREE.BoxGeometry(3, 0.5, 1), new THREE.MeshBasicMaterial({
-  color: 0x800080,
-  transparent: false,
-  wireframe: true
-  // opacity: 0.5
-}));
-scene.add(hallway);
+// a mutative function
+const flip = (arr, end) => {
+  let temp = undefined; // holds a value from the array
+  let start = 0;
 
+  while (start < end) {
+    temp = arr[start];
+    arr[start] = arr[end];
+    arr[end] = temp;
+    start++;
+    end--;
+  }
+};
 
+// a non-mutative function
+const pancakeSort = (original) => {
+  const arr = original.slice();
+
+  for (let i = arr.length; i > 1; i--) {
+    const maxIndex = arr.reduce((iterator, value, index, arr) => {
+      return (arr[iterator] > arr[index]) ? iterator : index;
+    }, 0);
+
+    if (maxIndex !== (i - 1)) {
+      flip(arr, maxIndex);
+      flip(arr, (i - 1));
+    }
+  }
+
+  return arr;
+};
+
+console.log('original: ' + JSON.stringify(arr, '', 2));
+console.log('sorted: ' + JSON.stringify(pancakeSort(arr), '', 2));
+console.log('arr: ' + JSON.stringify(arr, '', 2));
 
 function animate() {
   requestAnimationFrame(animate);
 
+  // flipping logic here
+
   renderer.render(scene, camera);
 }
 animate();
+
+
 

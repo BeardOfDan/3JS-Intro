7d58deeecb28286f891496dadf4845d0ff2440f7
Daniel Roberts <BeardOfDan@gmail.com>
2018-10-02 14:30:53 -0700

    (feat) Write the createSpheres function

--- js/scratch.js
+++ js/scratch.js
@@ -8,6 +8,38 @@ const container = document.getElementById('container');
 // Helper var which we will use as a additional correction coefficient for objects and camera
 let distance = 400;
 
+
+const createSpheres = () => {
+  const spheres = new THREE.Object3D();
+
+  // randomly generate spheres
+  const numSpheres = 80;
+  for (let i = 0; i < numSpheres; i++) {
+    const sphere = new THREE.SphereGeometry(4, Math.random() * 12, Math.random() * 12);
+    const material = new THREE.MeshPhongMaterial({
+      color: (Math.random() * 0xff0000) - 0xff0000,
+      shading: THREE.FlatShading
+    });
+
+    const particle = new THREE.Mesh(sphere, material);
+    particle.position.x = Math.random() * distance * 10;
+    particle.position.y = Math.random() * -distance * 6;
+    particle.position.z = Math.random() * distance * 4;
+    particle.rotation.y = Math.random() * 2 * Math.PI;
+    particle.scale.x = particle.scale.y = particle.scale.z = Math.random() * 12 + 5;
+
+    spheres.add(particle);
+  }
+
+  spheres.position.y = 500;
+  spheres.position.x = -2000;
+  spheres.position.z = -100;
+  spheres.rotation.y = Math.PI * 600;
+
+  scene.add(spheres);
+}; // end of createSpheres
+
+
 // const init = () => {
 const renderer = new THREE.WebGLRenderer({ antialias: true });
 renderer.setSize(window.innerWidth, window.innerHeight);
@@ -28,11 +60,15 @@ lightTwo.position.set(-100, 800, 800);
 const lightAmbient = new THREE.AmbientLight(0x404040);
 scene.add(light, lightTwo, lightAmbient);
 
-// createSpheres();
+createSpheres();
 // createDiamond();
 // createSpace();
 
 renderer.render(scene, camera);
 // }; // end of init
+
+
+
+
 
 
